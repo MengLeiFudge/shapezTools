ccd14be206b0cbd9bbe50ef2fdcdc0b23a6efd35
萌泪酱 <605738729@qq.com>
2022-08-22 00:10:59 +0800

    Finish step1.

--- src/main/java/shapez/calculate/GetAllShapes.java
+++ src/main/java/shapez/calculate/GetAllShapes.java
@@ -1,5 +1,8 @@
 package shapez.calculate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -9,111 +12,99 @@ import java.util.HashSet;
  * 由于并不需要图形每个角的形状、颜色，只需要结构，
  * 程序中将使用 1-65535 的 id 表示结构，每个 bit 表示一个角是否存在。
  * 由于java进行位运算时，会先转换成int，所以id使用int而非。
+ * <p>
+ * 1.获取所有可合成图形的id
+ * <p>
+ * 2.根据第一步的id，获取任一一个id的所有“上一步”
  *
  * @author MengLeiFudge
  */
 public class GetAllShapes {
-    /**
-     * 指示某个id对应的图形是否存在.
-     */
-    private final boolean[] exists = new boolean[65536];
 
+    private static final Logger logger = LoggerFactory.getLogger(GetAllShapes.class);
 
-    /**
-     * steps不为-1，表示id存在
-     */
-    private int[] steps;
-    Operate[] operates;
-    int[] parent1;
-    int[] parent2;
-
-
-    /**
-     * 指示图形合成所需最少步骤的 map.
-     * <p>
-     * key 表示该图形合成所需最少步骤，value 表示该图形。
-     */
-    private final HashMap<Integer, SimpleShape> map = new HashMap<>();
-
-
-    private final HashSet<Integer> set = new HashSet<>();
-
+    //private final
 
     public GetAllShapes() {
-        for (int i = 1; i < 0b1111; i++) {
-           // map.put(1, i);
-            set.add(i);
-        }
     }
 
 
     public void process() {
         long t1 = System.currentTimeMillis();
-        int step = 1;
+
+        getAllShapes();
+
+        long t2 = System.currentTimeMillis();
+        System.out.println("用时 " + (t2 - t1) / 1000.0 + " s");
+
+
+    }
+
+    /**
+     * 获取所有的可合成图形id.
+     */
+    private void getAllShapes() {
+        // 指示获取某个图形需要的最少步骤。索引为id的steps不为0，表示id存在。
+        int[] steps = new int[0x10000];
+        // 可合成图形总数
+        int num = 0;
+        for (int i = 0x0001; i <= 0x000f; i++) {
+            steps[i] = 1;
+            num++;
+        }
+        int currentStep = 1;
         while (true) {
             boolean updated = false;
-            for (int id = 1; id <= 65535; id++) {
-                if (steps[id] != step) {
+            // todo: 加多线程处理
+            for (int id = 0x0001; id <= 0xffff; id++) {
+                if (steps[id] != currentStep) {
                     continue;
                 }
                 SimpleShape shape = new SimpleShape(id);
-                int r90 = shape.process(Operate.R90).getId();
-                if (!exists[r90]) {
-                    steps[r90] = step + 1;
-                    operates[r90] = Operate.R90;
-                    parent1[r90] = id;
-                    updated = true;
-                }
-                int left = shape.process(Operate.LEFT).getId();
-                if (left != 0 && !exists[left]) {
-                    steps[left] = step + 1;
-                    operates[left] = Operate.LEFT;
-                    parent1[left] = id;
-                    updated = true;
-                }
-                int right = shape.process(Operate.RIGHT).getId();
-                if (right != 0 && !exists[right]) {
-                    steps[right] = step + 1;
-                    operates[right] = Operate.RIGHT;
-                    parent1[right] = id;
-                    updated = true;
-                }
-                for (int id2 = 1; id2 <= 65535; id2++) {
-                    if (!exists[id2]) {
-                        continue;
+                for (var x : Operate.values()) {
+                    if (x != Operate.STACK) {
+                        SimpleShape shapeNew = shape.process(x);
+                        int newId = shapeNew.getId();
+                        if (newId != 0 && steps[newId] == 0) {
+                            steps[newId] = currentStep + 1;
+                            updated = true;
+                            num++;
+                            //logger.info(shape.getIdStr() + " -> " + x + " -> " + shapeNew.getIdStr());
+                        }
+                    } else {
+                        for (int id2 = 0x0001; id2 <= 0xffff; id2++) {
+                            if (steps[id2] == 0) {
+                                continue;
+                            }
+                            SimpleShape shape2 = new SimpleShape(id2);
+                            SimpleShape shapeNew1 = shape.process(Operate.STACK, shape2);
+                            int stack1 = shapeNew1.getId();
+                            if (steps[stack1] == 0) {
+                                steps[stack1] = currentStep + 1;
+                                updated = true;
+                                num++;
+                                //logger.info(shape.getIdStr() + " on " + shape2.getIdStr() + " -> " + shapeNew1.getIdStr());
+                            }
+                            SimpleShape shapeNew2 = shape2.process(Operate.STACK, shape);
+                            int stack2 = shapeNew2.getId();
+                            if (steps[stack2] == 0) {
+                                steps[stack2] = currentStep + 1;
+                                updated = true;
+                                num++;
+                                //logger.info(shape2.getIdStr() + " on " + shape.getIdStr() + " -> " + shapeNew2.getIdStr());
+                            }
+                        }
                     }
- /*                   int stack1 = stack(id, id2);
-                    if (!exists[stack1]) {
-                        steps[stack1] = step + 1;
-                        operates[stack1] = Operate.ADD;
-                        parent1[stack1] = id;
-                        parent2[stack1] = id2;
-                        num++;
-                        show(stack1);
-                        updated = true;
-                    }
-                    int stack2 = stack(id2, id);
-                    if (!exists[stack2]) {
-                        steps[stack2] = step + 1;
-                        operates[stack2] = Operate.ADD;
-                        parent1[stack2] = id2;
-                        parent2[stack2] = id;
-                        num++;
-                        show(stack2);
-                        updated = true;
-                    }*/
                 }
             }
             if (updated) {
-                //System.out.println("Step" + step + " End, num: " + num);
-                step++;
+                System.out.println("Step" + currentStep + " End, num: " + num);
+                currentStep++;
             } else {
-                //System.out.println("Finish, num: " + num);
+                System.out.println("Finish, num: " + num);
                 break;
             }
         }
-        long t2 = System.currentTimeMillis();
-        System.out.println("用时 " + (t2 - t1) / 1000.0 + " s");
     }
 
 
--- src/main/java/shapez/calculate/SimpleShape.java
+++ src/main/java/shapez/calculate/SimpleShape.java
@@ -2,6 +2,11 @@ package shapez.calculate;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 表示一个没有角的形状、颜色信息，只有角存在与否信息的图形.
  *
@@ -23,11 +28,16 @@ public class SimpleShape {
         this.id = id;
     }
 
-    private static final Operate[] db2C1R = Operate.values2c1r();
-    private static final Operate[] dbFull = Operate.values();
+    private HashMap<Operate, ArrayList<SimpleShape>> operateMap = new HashMap<>();
 
 
-    //private ConcurrentHashMap<Operate, ArrayList<SimpleShape>> operateMap = new ConcurrentHashMap<>();
+    private ArrayList<SimpleShape> leftCutList = new ArrayList<>();
+    //private ArrayList<SimpleShape> leftCutList = new ArrayList<>();
+    //private ArrayList<SimpleShape> leftCutList = new ArrayList<>();
+    private HashMap<SimpleShape, SimpleShape> stackMap = new HashMap<>();
+
+
+    // region 正向处理
 
     public SimpleShape process(Operate operate, SimpleShape... shape) {
         return switch (operate) {
@@ -56,70 +66,83 @@ public class SimpleShape {
         switch (angle) {
             case 90 -> {
                 //0x7777: 0111 0111 0111 0111  0x8888: 1000 1000 1000 1000
-                return new SimpleShape(((id & 0x7777) << 1) | ((id & 0x8888) >> 3));
+                return new SimpleShape(((id & 0x7777) << 1) | ((id & 0x8888) >>> 3));
             }
             case 180 -> {
                 //0x3333: 0011 0011 0011 0011  0xCCCC: 1100 1100 1100 1100
-                return new SimpleShape(((id & 0x3333) << 2) | ((id & 0xCCCC) >> 2));
+                return new SimpleShape(((id & 0x3333) << 2) | ((id & 0xCCCC) >>> 2));
             }
             case 270 -> {
                 //0x1111: 0001 0001 0001 0001  0xEEEE: 1110 1110 1110 1110
-                return new SimpleShape(((id & 0x1111) << 1) | ((id & 0xEEEE) >> 3));
+                return new SimpleShape(((id & 0x1111) << 3) | ((id & 0xEEEE) >>> 1));
             }
             default -> throw new IllegalArgumentException("Invalid angle: " + angle);
         }
     }
 
+    /**
+     * 返回切割后的图形.
+     *
+     * @param quadrants 需要保留的象限
+     * @return 切割后的图形
+     */
     private SimpleShape cut(int... quadrants) {
         if (quadrants == null || quadrants.length == 0) {
-            throw new IllegalArgumentException("No quadrants specified");
+            throw new IllegalArgumentException("象限未定义");
         }
-        int[] quadrant = new int[quadrants.length];
-        //for
-
-        return new SimpleShape(0);
+        int filter = 0;
+        for (var x : quadrants) {
+            if (x < 1 || x > 4) {
+                throw new IllegalArgumentException("象限不是1-4");
+            }
+            filter |= (0b0001 << (x - 1));
+        }
+        // AX的速度更快一些
+        return cutAX(filter);
     }
 
-    private SimpleShape leftSideBC() {
-        //0xCCCC: 1100 1100 1100 1100，取出左半部分
+    /**
+     * BC提供的切割方法.
+     *
+     * @param filter 层筛选器
+     * @return 根据层筛选器切割后的最简图形的id
+     */
+    private SimpleShape cutBC(int filter) {
         int id = this.id;
         int ret = 0;
-        //层筛选器
-        int filter = 0xC;
         while (id != 0) {
-            //取出指定层
+            // 取出指定层
             int temp = id & filter;
             if (temp > 0) {
-                //该层非空，该层直接添加到ret里面，层筛选器上移
+                // 该层非空，该层直接添加到ret里面，层筛选器上移
                 ret |= temp;
                 filter <<= 4;
             } else {
-                //该层空，原图形砍掉底层
-                id >>= 4;
+                // 该层空，原图形砍掉底层
+                id >>>= 4;
             }
         }
         return new SimpleShape(ret);
     }
 
     /**
-     * 最好使用该方法，因为它稍快一些.
+     * AX提供的切割方法，稍快一些.
      *
-     * @return
+     * @param filter 层筛选器
+     * @return 根据层筛选器切割后的最简图形的id
      */
-    private SimpleShape leftSideAX() {
+    private SimpleShape cutAX(int filter) {
         int ret = 0;
-        //层筛选器
-        int filter = 0xC;
-        //空层数目
+        // 空层数目
         int q = 0;
         for (int i = 0; i < 4; i++) {
-            //取出指定层
+            // 取出指定层
             int temp = id & filter;
             if (temp > 0) {
-                //该层非空，该层右移空层数后添加到ret里面
-                ret |= temp >> q;
+                // 该层非空，该层右移空层数后添加到ret里面
+                ret |= temp >>> q;
             } else {
-                //该层空，空层计数增加
+                // 该层空，空层计数增加
                 q += 4;
             }
             //层筛选器上移
@@ -129,48 +152,175 @@ public class SimpleShape {
     }
 
     /**
-     * 三切（去掉右上角），only for mod.
+     * 返回当前图形堆叠在传入参数对应图形上的图形.
      *
-     * @param id
-     * @return
+     * @param bottomShape 堆叠的底层图形
+     * @return 堆叠后的图形
      */
-    private SimpleShape cutBlac() {
+    private SimpleShape stackOn(SimpleShape bottomShape) {
+        // 上层图形放到高16位
+        int top = this.id << 16;
+        int bottom = bottomShape.id;
+        // 将上层图形下移，直至碰到地板（次数到达4次），或者上下图形重合
+        for (int i = 0; i < 4; i++) {
+            // 必须用无符号右移>>>
+            top >>>= 4;
+            // 如果有重合部分，则将上层图形上移
+            if ((bottom & top) != 0) {
+                top <<= 4;
+                break;
+            }
+        }
+        // 上层图形除去高于四层的部分后，与下层图形合到一起
+        return new SimpleShape(bottom | (top & 0xFFFF));
+    }
+
+    // endregion
+
+    // region 反向处理
+
+/*    public List<SimpleShape> process(Operate operate, SimpleShape... shape) {
+        return switch (operate) {
+            case LEFT -> cut(3, 4);
+            case RIGHT -> cut(1, 2);
+            case TOP_RIGHT -> cut(1);
+            case BOTTOM_RIGHT -> cut(2);
+            case BOTTOM_LEFT -> cut(3);
+            case TOP_LEFT -> cut(4);
+            case R90 -> rotate(90);
+            case R180 -> rotate(180);
+            case R270 -> rotate(270);
+            case STACK -> stackOn(shape[0]);
+        };
+    }*/
+
+    /**//**
+     * 返回旋转后的图形.
+     * <p>
+     * 以顺时针旋转 90 度为例，应该将每层表示右上、右下、左下的 bit 左移 1 位，表示左上的 bit 右移 3 位。
+     *
+     * @param angle 旋转角度
+     * @return 旋转后的图形
+     *//*
+    private SimpleShape rotate(int angle) {
+        switch (angle) {
+            case 90 -> {
+                //0x7777: 0111 0111 0111 0111  0x8888: 1000 1000 1000 1000
+                return new SimpleShape(((id & 0x7777) << 1) | ((id & 0x8888) >>> 3));
+            }
+            case 180 -> {
+                //0x3333: 0011 0011 0011 0011  0xCCCC: 1100 1100 1100 1100
+                return new SimpleShape(((id & 0x3333) << 2) | ((id & 0xCCCC) >>> 2));
+            }
+            case 270 -> {
+                //0x1111: 0001 0001 0001 0001  0xEEEE: 1110 1110 1110 1110
+                return new SimpleShape(((id & 0x1111) << 3) | ((id & 0xEEEE) >>> 1));
+            }
+            default -> throw new IllegalArgumentException("Invalid angle: " + angle);
+        }
+    }
+
+    *//**
+     * 返回切割后的图形.
+     *
+     * @param quadrants 需要保留的象限
+     * @return 切割后的图形
+     *//*
+    private SimpleShape cut(int... quadrants) {
+        if (quadrants == null || quadrants.length == 0) {
+            throw new IllegalArgumentException("象限未定义");
+        }
+        int filter = 0;
+        for (var x : quadrants) {
+            if (x < 1 || x > 4) {
+                throw new IllegalArgumentException("象限不是1-4");
+            }
+            filter |= (0b0001 << (x - 1));
+        }
+        // AX的速度更快一些
+        return cutAX(filter);
+    }
+
+    *//**
+     * BC提供的切割方法.
+     *
+     * @param filter 层筛选器
+     * @return 根据层筛选器切割后的最简图形的id
+     *//*
+    private SimpleShape cutBC(int filter) {
+        int id = this.id;
         int ret = 0;
-        int filter = 0xE;
+        while (id != 0) {
+            // 取出指定层
+            int temp = id & filter;
+            if (temp > 0) {
+                // 该层非空，该层直接添加到ret里面，层筛选器上移
+                ret |= temp;
+                filter <<= 4;
+            } else {
+                // 该层空，原图形砍掉底层
+                id >>>= 4;
+            }
+        }
+        return new SimpleShape(ret);
+    }
+
+    *//**
+     * AX提供的切割方法，稍快一些.
+     *
+     * @param filter 层筛选器
+     * @return 根据层筛选器切割后的最简图形的id
+     *//*
+    private SimpleShape cutAX(int filter) {
+        int ret = 0;
+        // 空层数目
         int q = 0;
         for (int i = 0; i < 4; i++) {
-            if ((id & filter) > 0) {
-                ret |= (id & filter) >> q;
+            // 取出指定层
+            int temp = id & filter;
+            if (temp > 0) {
+                // 该层非空，该层右移空层数后添加到ret里面
+                ret |= temp >>> q;
             } else {
+                // 该层空，空层计数增加
                 q += 4;
             }
+            //层筛选器上移
             filter <<= 4;
         }
         return new SimpleShape(ret);
     }
 
-    private SimpleShape stackOn(SimpleShape shape) {
-   /*     int id1; int id2;
-        //右输入图形放到高16位，表示在上面
-        id2 <<= 16;
-        //如果未重合，就一直向下移，直到id2碰到地板，或者id2与id1重合
-        int i = 0;
-        while ((id1 & id2) == 0 && i < 4) {
-            id2 >>= 4;
-            i++;
-        }
-        //如果重合，把id2上移一层
-        if ((id1 & id2) > 0) {
-            id2 <<= 4;
-        }
-        //除去高于四层的部分
-        return id1 | (id2 & 0xFFFF);*/
-        return new SimpleShape(0);
-    }
+    */
 
+    /**
+     * 返回当前图形堆叠在传入参数对应图形上的图形.
+     *
+     * @param bottomShape 堆叠的底层图形
+     * @return 堆叠后的图形
+     *//*
+    private SimpleShape stackOn(SimpleShape bottomShape) {
+        // 上层图形放到高16位
+        int top = this.id << 16;
+        int bottom = bottomShape.id;
+        // 将上层图形下移，直至碰到地板（次数到达4次），或者上下图形重合
+        for (int i = 0; i < 4; i++) {
+            // 必须用无符号右移>>>
+            top >>>= 4;
+            // 如果有重合部分，则将上层图形上移
+            if ((bottom & top) != 0) {
+                top <<= 4;
+                break;
+            }
+        }
+        // 上层图形除去高于四层的部分后，与下层图形合到一起
+        return new SimpleShape(bottom | (top & 0xFFFF));
+    }*/
 
-    private void show() {
-        //System.out.println(Integer.toBinaryString(id));
+    // endregion
+    public String getIdStr() {
+        String full = Integer.toBinaryString(id | 0x00010000).substring(1);
+        return full.substring(0, 4) + " " + full.substring(4, 8) + " " + full.substring(8, 12) + " " + full.substring(12);
     }
 
 }
